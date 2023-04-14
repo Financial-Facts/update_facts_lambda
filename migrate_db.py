08b7f38e55729173d4ef55f2b60c77d9bd495fd9
Choochera <gabrielmatthew20@gmail.com>
2023-04-14 17:03:49 -0500

    Upload to S3 bucket on insert/update

--- migrate_db.py
+++ migrate_db.py
@@ -9,40 +9,66 @@ from io import BytesIO
 from urllib.request import Request, urlopen
 from zipfile import ZipFile
 import shutil
-import filecmp
+import boto3
+from botocore.exceptions import ClientError
+import threading
 
+
+MAX_CONNECTION_REESTABLISH_ATTEMPTS = 5
+BUCKET_NAME = 'public-financial-facts-bucket-dev-1'
+
+def __initialize_S3() -> None:
+    return boto3.resource(
+        service_name='s3',
+        region_name='us-east-1',
+        aws_access_key_id=os.environ[const.S3_KEY],
+        aws_secret_access_key=os.environ[const.S3_SECRET_KEY])
 
 def __get_db_auth() -> list[str]:
     try:
         db_username: str = os.environ[const.DB_USERNAME_KEY],
         db_password: str = os.environ[const.DB_PASSWORD_KEY]
+        db_endpoint: str = os.environ[const.DB_ENDPOINT_KEY]
     except KeyError:
         raise Exception("Database credentials not provided")
-    return [db_username, db_password]
+    return [db_username[0], db_password, db_endpoint]
 
 
 def __get_db_connection():
-    print("Establishing database connection...")
     creds: list[str] = __get_db_auth()
     conn = psycopg2.connect(
-        host=const.HOST,
-        database=const.DATABASE_NAME,
-        user=creds[0][0],
+        host=creds[2], 
+        port=const.PORT,
+        database=const.DBNAME,
+        user=creds[0],
         password=creds[1],
-        options="-c search_path=financial_facts")
+        sslrootcert='SSLCERTIFICATE',
+        options="-c search_path=financial_facts",)
     return conn
 
 
-def __initialize_db(connection, cursor) -> None:
+def __initialize_db() -> None:
     print("Initializing database connection..")
-    cursor.execute(const.CREATE_FACTS_TABLE_QUERY)
+    connection = __get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(const.CREATE_FACTS_TABLE_QUERY)
+    except (psycopg2.errors.InvalidSchemaName):
+        cursor.execute(const.CREATE_SCHEMA_QUERY)
+        cursor.execute(const.CREATE_FACTS_TABLE_QUERY)
     connection.commit()
+    cursor.close()
+    connection.close()
 
 
-def __drop_facts(connection, cursor) -> None:
+def __drop_facts() -> None:
     print("Dropping facts table from database...")
+    connection = __get_db_connection()
+    cursor = connection.cursor()
     cursor.execute(const.DROP_FACTS_TABLE_QUERY)
     connection.commit()
+    cursor.close()
+    connection.close()
 
 
 def __delete_data() -> None:
@@ -53,12 +79,9 @@ def __delete_data() -> None:
 
 def __download_data() -> None:
     print("Downloading facts data from EDGAR...")
-    try: 
-        os.mkdir('Data')
-    except FileExistsError:
-        pass
     try:
-        os.mkdir('Temp')
+        # os.mkdir('Temp')
+        pass
     except FileExistsError:
         # Process already started but not finished...
         print("Continuing with current temp folder...")
@@ -69,43 +92,75 @@ def __download_data() -> None:
     )
     with urlopen(req) as zipresp:
         with ZipFile(BytesIO(zipresp.read())) as zfile:
-            zfile.extractall(const.TEMP_DIRECTORY)
+            __process_data(zfile)
 
 
-def __process_data() -> list:
+def __process_data(zfile: ZipFile) -> list:
     print("Processing data updates..")
-    tempFiles = [
-            os.path.relpath(file) for file in os.listdir(const.TEMP_DIRECTORY)
-        ]
+    s3 = __initialize_S3()
     fileAdded = False
-    for i in range(len(tempFiles)):
-        with open('Temp\\' + tempFiles[i]) as openTempFile:
+    for file in zfile.filelist:
+        cik = file.filename[:-5]
+        try:
+            temp = json.loads(zfile.read(file))
             try:
-                temp = json.load(openTempFile)
-                try:
-                    with open('Data\\' + tempFiles[i]) as openDataFile:
-                        data = json.load(openDataFile)
-                        if (
-                            temp != data
-                        ):
-                            print("Updating %s..." % openTempFile.name)
-                            cik = openTempFile.name[5:-5]
-                            __update_database(temp, cik)
-                            fileAdded = True
-                except FileNotFoundError:
-                    cik = openTempFile.name[5:-5]
-                    print("Adding %s..." % openTempFile.name)
-                    __insert_database(temp, cik)
+                data = json.loads(s3.Bucket(BUCKET_NAME).Object(file.filename).get()['Body'].read().decode())
+                if (
+                    temp != data
+                ):
+                    print("Updating %s..." % cik)
+                    __attempt_update(cik, temp)
                     fileAdded = True
-            except json.decoder.JSONDecodeError:
-                print("Cannot process %s" % openTempFile.name)
+            except ClientError as ex:
+                if ex.response['Error']['Code'] == 'NoSuchKey':
+                    print("Adding %s..." % cik)
+                    __attempt_insert(cik, temp)
+                    fileAdded = True
+                else:
+                    print(ex)
+        except json.decoder.JSONDecodeError:
+            print("Cannot process %s" % cik)
         if (fileAdded):
-            os.replace('Temp\\' + tempFiles[i], 'Data\\' + tempFiles[i])
             fileAdded = False
-    shutil.rmtree('Temp')
+            object = s3.Object(BUCKET_NAME, file.filename)
+            object.put(Body=json.dumps(temp))
+
+def __attempt_insert(cik, data) -> None:
+    connection = __get_db_connection()
+    insertComplete = False
+    retryAttempts = MAX_CONNECTION_REESTABLISH_ATTEMPTS
+    with connection.cursor() as cursor:
+        while (not insertComplete and retryAttempts >= 0):
+            try:
+                __insert_database(data, cik, cursor)
+                insertComplete = True
+            except psycopg2.errors.InFailedSqlTransaction:
+                print("%s already exists within database..." % cik)
+                insertComplete = True
+            except psycopg2.OperationalError:
+                connection = __get_db_connection()
+                cursor = connection.cursor()
+    cursor.close()
+    connection.close()
 
 
-def __update_database(data, cik) -> None:
+def __attempt_update(cik, data) -> None:
+    connection = __get_db_connection()
+    updateComplete = False
+    retryAttempts = MAX_CONNECTION_REESTABLISH_ATTEMPTS
+    with connection.cursor() as cursor:
+        while (not updateComplete and retryAttempts >= 5):
+            try:
+                __update_database(data, cik, cursor)
+                updateComplete = True
+            except psycopg2.OperationalError:
+                connection = __get_db_connection()
+                cursor = connection.cursor()
+    cursor.close()
+    connection.close()
+
+
+def __update_database(data, cik, cursor) -> None:
     try:
         text = json.dumps(data)
         text = text.replace('\'', const.EMPTY)
@@ -120,7 +175,7 @@ def __update_database(data, cik) -> None:
         pass
 
 
-def __insert_database(data, cik) -> None:
+def __insert_database(data, cik, cursor) -> None:
     try:
         text = json.dumps(data)
         text = text.replace('\'', const.EMPTY)
@@ -138,8 +193,6 @@ def __insert_database(data, cik) -> None:
 if __name__ == '__main__':
 
     startTime = time.time()
-    connection = __get_db_connection()
-    cursor = connection.cursor()
 
     print("Checking repopulation flag status...")
     try:
@@ -152,17 +205,11 @@ if __name__ == '__main__':
         repopulateFlag = False
 
     if (repopulateFlag):
-        __drop_facts(connection, cursor)
+        __drop_facts()
         __delete_data()
-        connection.commit()
 
-    __initialize_db(connection, cursor)
+    __initialize_db()
     __download_data()
-    __process_data()
-
-    connection.commit()
-    cursor.close()
-    connection.close()
 
     end = time.time()
 
--- serviceConstants.py
+++ serviceConstants.py
@@ -1,7 +1,16 @@
+#DB
+ENDPOINT='dev-financial-facts-db-1-public-instance-1.cilvae7glcw6.us-east-1.rds.amazonaws.com'
+PORT="5432"
+REGION="us-east-1"
+DBNAME="financial_facts_db"
+DB_USERNAME_KEY="USERNAME_KEY"
+DB_PASSWORD_KEY="PASSWORD_KEY"
+DB_ENDPOINT_KEY="ENDPOINT_KEY"
+S3_KEY="S3_KEY"
+S3_SECRET_KEY="S3_SECRET_KEY"
+
 # Misc
-DB_USERNAME_KEY = 'db_username'
-DB_PASSWORD_KEY = 'db_password'
-HOST = 'localhost'
+HOST = 'dev-financial-facts-db-1-public-instance-1.cilvae7glcw6.us-east-1.rds.amazonaws.com'
 DATABASE_NAME = 'postgres'
 USER_AGENT_VALUE = 'XYZ/3.0'
 DATA_DIRECTORY = 'Data'
@@ -22,11 +31,14 @@ FINANCIAL_FACTS = 'financial_facts'
 # Queries
 GET_PROCESSED_CIK_QUERY = 'SELECT cik from facts;'
 DROP_FACTS_TABLE_QUERY = 'DROP TABLE IF EXISTS facts;'
+CREATE_SCHEMA_QUERY = """CREATE SCHEMA financial_facts;
+                         GRANT ALL ON SCHEMA financial_facts TO choochera;
+                         SET schema 'financial_facts'"""
 CREATE_FACTS_TABLE_QUERY = """CREATE TABLE IF NOT EXISTS facts (
                             cik varchar(13) not null primary key,
                             data jsonb
                         );"""
-INSERT_DATA_QUERY = """INSERT INTO facts (cik, data)
+INSERT_DATA_QUERY = """INSERT INTO financial_facts.facts (cik, data)
                      values('%s', (select * from to_jsonb('%s'::JSONB)));COMMIT;"""
 UPDATE_DATA_QUERY = """UPDATE facts set data='%s' where cik='%s';COMMIT;
 """
