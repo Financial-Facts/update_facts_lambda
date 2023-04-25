3a8ab95dd18a23f2065f23ad7ffb9703dc6ab93d
Choochera <gabrielmatthew20@gmail.com>
2023-04-25 01:55:39 -0500

    Lambda configuration/runnable

--- connection_utils.py
+++ connection_utils.py
@@ -4,17 +4,11 @@ import psycopg2
 import boto3
 from base64 import b64decode
 
-def decrypt_env_variable(encrypted: str):
-    return boto3.client('kms').decrypt(
-        CiphertextBlob=b64decode(encrypted),
-        EncryptionContext={'LambdaFunctionName': os.environ['AWS_LAMBDA_FUNCTION_NAME']}
-    )['Plaintext'].decode('utf-8')
-
 def get_db_auth() -> list[str]:
     try:
-        db_username: str = decrypt_env_variable(os.environ[const.DB_USERNAME_KEY])
-        db_password: str = decrypt_env_variable(os.environ[const.DB_PASSWORD_KEY])
-        db_endpoint: str = decrypt_env_variable(os.environ[const.DB_ENDPOINT_KEY])
+        db_username: str = os.environ[const.DB_USERNAME_KEY]
+        db_password: str = os.environ[const.DB_PASSWORD_KEY]
+        db_endpoint: str = os.environ[const.DB_ENDPOINT_KEY]
     except KeyError:
         raise Exception("Database credentials not provided")
     return [db_username, db_password, db_endpoint]
@@ -28,12 +22,12 @@ def get_db_connection():
         user=creds[0],
         password=creds[1],
         sslrootcert='SSLCERTIFICATE',
-        options="-c search_path=financial_facts",)
+        options="-c search_path=financial_facts")
     return conn
 
 def initialize_S3():
     return boto3.resource(
         service_name='s3',
         region_name='us-east-1',
-        aws_access_key_id=decrypt_env_variable(os.environ[const.S3_KEY]),
-        aws_secret_access_key=decrypt_env_variable(os.environ[const.S3_SECRET_KEY]))
+        aws_access_key_id=os.environ[const.S3_KEY],
+        aws_secret_access_key=os.environ[const.S3_SECRET_KEY])
--- migrate_db.py
+++ migrate_db.py
@@ -1,19 +1,17 @@
 import psycopg2
 import time
-import json
+import os
 import serviceConstants as const
 from io import BytesIO
 from urllib.request import Request, urlopen
 from zipfile import ZipFile
 from zipfile import ZipInfo
-from botocore.exceptions import ClientError
 import file_processing_worker as fpw
 import connection_utils as utils
 from queue import Queue
 
 
 MAX_NUMBER_OF_THREADS = 10
-
 
 def __initialize_db() -> None:
     print("Initializing database connection..")
@@ -29,22 +27,27 @@ def __initialize_db() -> None:
     connection.close()
 
 
-def __download_and_process_data() -> None:
+def __download_and_process_data(number_of_chunks: int, chunk_to_process: int) -> None:
     print("Downloading facts data from EDGAR...")
     req = Request(
         url=const.EDGAR_URL + const.DATA_ZIP_PATH,
-        headers={'User-Agent': const.USER_AGENT_VALUE}
+        headers={
+            'User-Agent': os.environ[const.USER_AGENT_VALUE_KEY],
+            'Accept-Encoding': 'gzip, deflate',
+            'Host': 'www.sec.gov'
+        }
     )
     with urlopen(req) as zipresp:
         with ZipFile(BytesIO(zipresp.read())) as zfile:
-            __divide_processing_workload(zfile)
+            __divide_processing_workload(number_of_chunks, chunk_to_process, zfile)
 
 
-def __divide_processing_workload(zfile: ZipFile):
+def __divide_processing_workload(number_of_chunks: int, chunk_to_process: int, zfile: ZipFile):
     print("Dividing processing workload to workers...")
     files: list[ZipInfo] = zfile.filelist
     queue = Queue()
-    for file in files:
+    chunks = __separate_into_chunks(files, number_of_chunks)
+    for file in chunks[chunk_to_process]:
         queue.put(file)
     threads = []
     s3Client = utils.initialize_S3()
@@ -62,13 +65,26 @@ def __divide_processing_workload(zfile: ZipFile):
         t.join()
     del (threads)
 
+def __separate_into_chunks(files, num):
+    avg = len(files) / float(num)
+    out = []
+    last = 0.0
 
-if __name__ == '__main__':
+    while last < len(files):
+        out.append(files[int(last):int(last + avg)])
+        last += avg
 
+    return out
+
+def start(event, context):
+    print("Starting facts update...")
     startTime = time.time()
+    
+    number_of_chunks = int(event['NUMBER_OF_CHUNKS_KEY'])
+    chunk_to_process = int(event['CHUNK_TO_PROCESS_KEY'])
 
     __initialize_db()
-    __download_and_process_data()
+    __download_and_process_data(number_of_chunks, chunk_to_process)
 
     end = time.time()
 
